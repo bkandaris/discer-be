a036e34f9ed3c068f0f29bdca1d6fa0af1fb45f2
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-12-07 10:29:24 -0500

    updated users route

--- routes/users.js
+++ routes/users.js
@@ -16,6 +16,10 @@ router.post('/register', async (req, res) => {
     const newUser = new User({
       username: req.body.username,
       password: hashedPassword,
+      email: req.body.email,
+      phone: req.body.phone,
+      skill: req.body.skill,
+      profilePicture: req.body.profilePicture,
     });
     const user = await newUser.save();
     res.status(200).json(user);
