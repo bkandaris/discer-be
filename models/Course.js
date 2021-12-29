c9a4571390d6ca612d92b8e65ea4f62afb439ab8
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-12-29 14:52:22 -0500

    updated course schema

--- models/Course.js
+++ models/Course.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const CourseSchema = new mongoose.Schema(
   {
     courseName: { type: String, required: true, unique: true },
-    courseLocation: { type: String, required: true },
+    courseAddress: { type: String, required: true },
+    courseCity: { type: String, required: true },
+    courseState: { type: String, required: true },
     description: { type: String, required: false },
-    coursePicture: { type: String, required: true },
+    coursePicture: { type: String, required: false },
   },
   { timestamps: true }
 );
