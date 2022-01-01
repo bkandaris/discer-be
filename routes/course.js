e4919f645b803936ddbc9668e79b02630224451e
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2022-01-01 13:20:52 -0500

    fixed route

--- routes/course.js
+++ routes/course.js
@@ -6,7 +6,7 @@ router.post('/', async (req, res) => {
   const newCourse = new Course(req.body);
   try {
     const savedCourse = await newCourse.save();
-    res.status(200).json(savedCart);
+    res.status(200).json(savedCourse);
   } catch (err) {
     res.status(500).json(err);
   }
