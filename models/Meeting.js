5a1cdc4537c07d47fcb82fd4fce3dda241255eb0
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2022-06-10 18:26:59 -0400

    hopefully last update to meeting schema

--- models/Meeting.js
+++ models/Meeting.js
@@ -5,7 +5,7 @@ const MeetingSchema = new mongoose.Schema(
     createdById: { type: String, required: true },
     meetingCourse: { type: String, required: true },
     meetingDescription: { type: String, required: true },
-    meetingDate: { type: Object, required: true },
+    meetingDate: { type: String, required: true },
     meetingTime: { type: String, required: true },
     meetingPlayers: { type: Array, required: false },
   },
