7167b7065dd50dfe10e207891fe07c8ee59c6b66
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2022-06-14 11:33:38 -0400

    added routes for get meetups and single meetup

--- routes/meeting.js
+++ routes/meeting.js
@@ -1,6 +1,26 @@
 const router = require('express').Router();
 const Meetup = require('../models/Meeting');
 
+// get meetups
+router.get('/', async (req, res) => {
+  try {
+    const meetups = await Meetup.find();
+    res.status(200).json(meetups);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// single meetup
+router.get('/find/:courseId', async (req, res) => {
+  try {
+    const meetup = await Meetup.findById(req.params.meetupId);
+    res.status(200).json(meetup);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // create meetup
 router.post('/', async (req, res) => {
   const newMeetup = new Meetup(req.body);
